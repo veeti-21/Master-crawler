6d90b769732cb374dfe100e950bf2675788a3cf8
veeti-21 <158137597+veeti-21@users.noreply.github.com>
2025-11-10 10:15:15 +0200

    tein web crawlerin joka kopio koko nettimökin htmllän

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import urllib.parse
+
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                  "(KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+}
+
+
+BASE_URL = "https://www.nettimokki.com/vuokramokit/mokit-jarven-rannalla/"
+PARAMS = {
+    "item_availability__date_from": "2026-06-01",
+    "item_availability__date_to": "2026-06-07"
+}
+
+def fetch_page(params=PARAMS, base_url=BASE_URL):
+    if params:
+        query_string = urllib.parse.urlencode(params, safe='/', encoding='utf‑8')
+        url = f"{base_url}?{query_string}"
+    else:
+        url = base_url
+    print(f"Fetching URL: {url}")
+    try:
+        resp = requests.get(url, headers=HEADERS, timeout=10)
+        resp.raise_for_status()
+        html = resp.text
+        print(html)  
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching {url}: {e}")
+
+if __name__ == "__main__":
+
+    fetch_page()
