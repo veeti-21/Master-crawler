7b28a0aec72bfef91cf8b6f663d67e1e4df88856
moiasdkj <officialdeluxo@gmail.com>
2025-11-11 08:38:18 +0200

    Create input_date function and add gitignore
    def input_date(date_from, date_to): # format vvvv-kk-pv
    
        global PARAMS
    
        PARAMS.update = {
        "item_availability__date_from": date_from,
        "item_availability__date_to": date_to
        }

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import time
 import urllib.parse
 
 
+FILE_LOCATION = "C:\\Users\\jani\\Downloads\\useless-shit.txt"
+
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (compatible; VeetiBot/1.0; +https://example.com)"
 }
@@ -15,10 +17,22 @@ PARAMS = {
     "item_availability__date_to": "2026-06-07"
 }
 
+def input_date(date_from, date_to): # format vvvv-kk-pv
+
+    global PARAMS
+
+    PARAMS.update = {
+    "item_availability__date_from": date_from,
+    "item_availability__date_to": date_to
+    }
+    
+
 def fetch_page(params=PARAMS, base_url=BASE_URL):
     if params:
-        query_string = urllib.parse.urlencode(params, safe='/', encoding='utf‑8')
+        query_string = urllib.parse.urlencode(params, safe='/', encoding='utf-8')
         url = f"{base_url}?{query_string}"
+        print(query_string)
+        print(url)
     else:
         url = base_url
     print(f"Fetching URL: {url}")
@@ -26,9 +40,12 @@ def fetch_page(params=PARAMS, base_url=BASE_URL):
         resp = requests.get(url, headers=HEADERS, timeout=10)
         resp.raise_for_status()
         html = resp.text
-        print(html)  
+        with open(FILE_LOCATION, "w", encoding="utf-8") as f:
+            f.write(html)
+
     except requests.exceptions.RequestException as e:
         print(f"Error fetching {url}: {e}")
+
 
 if __name__ == "__main__":
 
