18b27c21dbc12376dab7d5e1f7a9af3007595417
TopiPelkonen <topi.pelkonen@student.eduko.fi>
2025-11-18 13:29:24 +0200

    vaihdoin tiedostojen nimiä, haun tulos appendaa tiedostojen loppuihin

--- crawler1.py
+++ crawler1.py
@@ -8,9 +8,9 @@ import json
 import re
 import datetime
 import calendar
-
+import time
 # webpage linkkien tilalla pitäisi toimia mikä tahansa gigantin sivu. 
-webPage = ['gigantti.fi/gaming/pelinaytot?f=30831%3A2560x1440%7C3840x2160%7C3440x1440' , 'gigantti.fi/gaming/pelinaytot' , 'gigantti.fi/gaming/pelinaytot/page-2']
+webPage = ['gigantti.fi/gaming/tietokonekomponentit/naytonohjaimet-gpu' , 'gigantti.fi/gaming/tietokonekomponentit/naytonohjaimet-gpu/page-2' , 'gigantti.fi/gaming/tietokonekomponentit/naytonohjaimet-gpu/page-3' , 'gigantti.fi/gaming/pelinaytot?f=30831%3A2560x1440%7C3840x2160%7C3440x1440' , 'gigantti.fi/gaming/pelinaytot' , 'gigantti.fi/gaming/pelinaytot/page-2']
 elements = []
 
 
@@ -19,9 +19,9 @@ def crawl(*args):
         # ottaa ajan, muuntaa sen epoch aikaan ja tallentaa tiedoston muodossa "result-xxxxxxxxxx.json". tiedoston tallennus tapahtuu kansioon jossa crawler on
         currtime = datetime.datetime.now()
         epoch_time = calendar.timegm(currtime.timetuple())
-        filename = f'result-{epoch_time}.json'
+        filename = f'result-{z}.json'
         opts = Options()
-        opts.add_argument("-headless")
+        opts.add_argument("--headless=new")
         driver = webdriver.Firefox(options=opts)
         print(f"{z} odotus")
 
@@ -35,6 +35,7 @@ def crawl(*args):
         required_height = driver.execute_script('return document.body.parentNode.scrollHeight')
         driver.set_window_size(required_width, required_height)
 
+        time.sleep(20)
         
         elementurl = WebDriverWait(driver, 30).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, "li.group > a:nth-child(2)")) #This is a dummy element
@@ -52,7 +53,7 @@ def crawl(*args):
         driver.save_screenshot(f'screenie{z}.png')
 
         print(webPage[z])
-        for x in range (len(elementprice)):
+        for x in range (len(elementname)):
                 print(x)
                 tmp1 = f"{elementname[x].get_attribute('textContent')}\n"
                 tmp2 = f"{elementprice[x].text}\n"
