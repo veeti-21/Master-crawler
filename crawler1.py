639aa9ff7fd539c1999b09586823fb02d09c2943
TopiPelkonen <topi.pelkonen@student.eduko.fi>
2025-11-20 13:27:17 +0200

    pitää korjata 'driver.find_element'

--- crawler1.py
+++ crawler1.py
@@ -11,7 +11,6 @@ import calendar
 import time
 # webpage linkkien tilalla pitäisi toimia mikä tahansa gigantin sivu. 
 webPage = ['gigantti.fi/gaming/tietokonekomponentit/naytonohjaimet-gpu' , 'gigantti.fi/gaming/tietokonekomponentit/naytonohjaimet-gpu/page-2' , 'gigantti.fi/gaming/tietokonekomponentit/naytonohjaimet-gpu/page-3' , 'gigantti.fi/gaming/pelinaytot?f=30831%3A2560x1440%7C3840x2160%7C3440x1440' , 'gigantti.fi/gaming/pelinaytot' , 'gigantti.fi/gaming/pelinaytot/page-2']
-elements = []
 
 
 def crawl(*args):
@@ -23,51 +22,60 @@ def crawl(*args):
         opts = Options()
         opts.add_argument("--headless=new")
         driver = webdriver.Firefox(options=opts)
-        print(f"{z} odotus")
-
-
+        print(f" crawlaus nro: {z + 1}")
         driver.get(f'https://www.{webPage[z]}')
-        #driver.get("https://www.gigantti.fi/gaming/pelinaytot")
-        #driver.get("https://www.gigantti.fi/gaming/pelinaytot/page-2")
-        original_size = driver.get_window_size()
+
+        # odottaa että eväste banner latautuu sivulle
+        elementurl = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "button.coi-banner__accept:nth-child(3)"))
+)
+        try:    # klikkaa eväste popupista "OK"
+                gdprbtn = driver.find_element(By.CSS_SELECTOR, "button.coi-banner__accept:nth-child(3)")
+                driver.execute_script("arguments[0].scrollIntoView(true);", gdprbtn)
+                gdprbtn.click()
+        except:
+                print("no gdpr button here")
+
         # ilman alempia selenium ei lataa kaikkia sivun elementtejä oikein
         required_width = driver.execute_script('return document.body.parentNode.scrollWidth')
         required_height = driver.execute_script('return document.body.parentNode.scrollHeight')
-        driver.set_window_size(required_width, required_height)
+        driver.set_window_size(required_width, required_height)              
 
-        time.sleep(20)
-        
-        elementurl = WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "li.group > a:nth-child(2)")) #This is a dummy element
-)
-        elementname = WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "li.group > a:nth-child(2) > div > div:nth-child(1) > h2:nth-child(2)")) #This is a dummy element
-)
-        elementprice = WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "li.group > a > div > div > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)")) #This is a dummy element
-)
-        
-        elementprice = driver.find_elements(By.CSS_SELECTOR, "li.group > a > div > div > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)")
-        elementurl = driver.find_elements(By.CSS_SELECTOR, "li.group > a:nth-child(2)")
+        #elementprice = driver.find_elements(By.CSS_SELECTOR, "li.group > a > div > div > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)")
+        #elementurl = driver.find_elements(By.CSS_SELECTOR, "li.group > a:nth-child(2)")
         elementname = driver.find_elements(By.CSS_SELECTOR, "li.group > a:nth-child(2) > div > div:nth-child(1) > h2:nth-child(2)") # selectorin "div" osasta piti ottaa nth-child pois että se hakee nimen ja hinnan oikein
+        # screenshot testausta varten, voi poistaa myöhemmin.
         driver.save_screenshot(f'screenie{z}.png')
 
-        print(webPage[z])
+        print(webPage[z] , "\n\n\n\n\n\n\n")
         for x in range (len(elementname)):
-                print(x)
-                tmp1 = f"{elementname[x].get_attribute('textContent')}\n"
-                tmp2 = f"{elementprice[x].text}\n"
-                tmp3 = f"{elementurl[x].get_attribute('href')}\n\n"
+                        # tmp1 = nimi
+                        # tpm2 = hinta
+                        # tmp3 = linkki
+                # x + 1 koska gigantin sivun elementtien index alkaa ykkösestä.
+                # ei toimi kaikilla sivuilla koska ensimmäinen elementti on erilainen pelinäyttöjen ja näytönohjainten sivuilla
+                tmp1 = driver.find_element(By.CSS_SELECTOR, f"li:nth-child({x + 1}) > a:nth-child(2) > div > div:nth-child(1) > h2:nth-child(2)").text 
+                tmp2 = driver.find_element(By.CSS_SELECTOR, f"li.group:nth-child({x + 1}) > a > div > div > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)").text
+                tmp3 = driver.find_element(By.CSS_SELECTOR, f"li.group:nth-child({x + 1}) > a:nth-child(2)").get_attribute('href')
+                #tmp1 = f"{elementname[x].get_attribute('textContent')}"
+                #tmp1 = f"{elementname[x].text}"
+                #tmp2 = f"{elementprice[x].text}\n"
+                #tmp3 = f"{elementurl[x].get_attribute('href')}\n\n"
 
-                with open(filename, 'a', encoding='utf-8') as f: # tekee tiedoston ja lisää siihen tulokset 
-                    f.write(tmp1)
-                    f.write(tmp2)
-                    f.write(tmp3)
-                    print(tmp1)
-                    print(tmp2)
-                    print(tmp3)
-                    #f.close()
-        # printit testausta varten
+                with open(filename, 'a', encoding='utf-8') as f: # tekee tiedoston ja lisää siihen tulokset. pitää vielä formatoida paremmin json/xml/csv muotoon.
+                        f.write(str(tmp1))
+                        #f.write("\n")
+                        f.write(str(tmp2))
+                        #f.write("\n")
+                        f.write(str(tmp3))
+                        #f.write("\n\n")
+                        print(tmp1)
+                        print(tmp2)
+                        print(tmp3)
+                        #TODO kaikki printit ja ylimääräiset importit pitää poistaa kun crawleri toimii
+                        #TODO pitää keksiä miten haetut tiedot siirretään tietokantaan/koontisivulle
+                        #TODO tietojen formatointi
+                        f.close()
             
         driver.quit()
 
