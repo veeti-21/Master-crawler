d5f75a48412eae0f86115498ee7e760bdf0d6b05
Lindroos-student <leonardo.lindroos@student.eduko.fi>
2025-12-03 13:24:12 +0200

    korjaa Json format
    Json format pitäisi olla toimiva
    Nimi ja Hinta ei vieläkään toimi

--- crawler-uusiAuto.py
+++ crawler-uusiAuto.py
@@ -14,7 +14,7 @@ currtime = datetime.datetime.now()
 epoch_time = calendar.timegm(currtime.timetuple())
 filename = f'result-{epoch_time}.json'
 opts = Options()
-opts.add_argument("-headless")
+opts.add_argument("--headless=new")
 driver = webdriver.Firefox(options=opts)
 original_size = driver.get_window_size()
 required_width = driver.execute_script('return document.body.parentNode.scrollWidth')
@@ -25,20 +25,18 @@ webPage = ["https://www.nettiauto.com/hakutulokset?haku=P62829718&page=1", "http
 
 
 for z in range(len(webPage)):
-        print(z)
         driver.get(f"{webPage[z]}")
-
-
         elementname = driver.find_elements(By.CSS_SELECTOR, "#listingData > div.grid-x.cell.list-body-new.total-upsell-ad > div > div.product-card__body > div.product-card__info > h2")
         elementprice = driver.find_elements(By.CSS_SELECTOR, '#listingData > div.grid-x.cell.list-body-new > div > div.product-card__body > div.product-card__info > div:nth-child(2) > div')
         elementlink = driver.find_elements(By.CSS_SELECTOR, '#listingData > div.grid-x.cell.list-body-new > div > a')
-
-
-        for x in range (len(elementname)):
+        
+        
+        for x in range(len(elementname)):
+                
                 tmp1 = f"{elementname[x].text}\n"
                 tmp2 = f"{elementprice[x].text}\n"
                 tmp3 = f"{elementlink[x].get_attribute('href')}\n\n"
-
+                
                 testjson = {
                 "nimi": tmp1,
                 "hinta": tmp2,
@@ -46,20 +44,22 @@ for z in range(len(webPage)):
                 }
 
                 with open(filename, 'a', encoding='utf-8') as f: # tekee tiedoston ja lisää siihen tulokset. pitää vielä formatoida paremmin json/xml/csv muotoon.
-                        if x == 0:
-                                f.writelines("{")
-                        print(json.dumps(testjson, indent=4, separators=(",", ":")))
-                        
-                        f.writelines(f'"objekti{x}"')
-                        f.writelines(":[")
-                        f.writelines(json.dumps(testjson, indent=4, separators=(",", ":")))
-                        if x == len(elementname) - 1:
-                                f.writelines("]\n}")
-                        else:
-                                f.writelines("],")
-
+                                if x == 0 and z == 0:
+                                        f.writelines("{")
+                                        
+                                        
+                                print(json.dumps(testjson, indent=4, separators=(",", ":")))
+                                        
+                                f.writelines(f'"objekti{x}-{z}"')
+                                f.writelines(":[")
+                                f.writelines(json.dumps(testjson, indent=4, separators=(",", ":")))
+                                if x == len(elementname) - 1 and z == 1:
+                                        f.writelines("]\n}")
+                                else:
+                                        f.writelines("],")
+                                
                 
-f.close()
+                f.close()
 
 driver.quit()
 #jostain syystä ei laita hintaa tai nimeä joillekin tuloksille
