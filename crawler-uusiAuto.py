5709327c0db20495af81b0fadb43978c5826a31f
Lindroos-student <leonardo.lindroos@student.eduko.fi>
2025-12-04 10:45:45 +0200

    lisää ja poista kommentteja
    lisää kommentteja

--- crawler-uusiAuto.py
+++ crawler-uusiAuto.py
@@ -20,21 +20,21 @@ original_size = driver.get_window_size()
 required_width = driver.execute_script('return document.body.parentNode.scrollWidth')
 required_height = driver.execute_script('return document.body.parentNode.scrollHeight')
 driver.set_window_size(required_width, required_height)
-
+# nettisivu
 webPage = ["https://www.nettiauto.com/hakutulokset?haku=P62829718&page=1", "https://www.nettiauto.com/hakutulokset?haku=P62829718&page=2"]
 
 with open(filename, 'w', encoding='utf-8') as f:# tekee tyhjän tai tyhjentää tiedoston
         f.writelines("")
 f.close()
 
-for z in range(len(webPage)):
+for z in range(len(webPage)):# etsii tulokset sivustosta
         driver.get(f"{webPage[z]}")
         elementname = driver.find_elements(By.CSS_SELECTOR, "#listingData > div.grid-x.cell.list-body-new.total-upsell-ad > div > div.product-card__body > div.product-card__info > h2")
         elementprice = driver.find_elements(By.CSS_SELECTOR, '#listingData > div.grid-x.cell.list-body-new > div > div.product-card__body > div.product-card__info > div:nth-child(2) > div')
         elementlink = driver.find_elements(By.CSS_SELECTOR, '#listingData > div.grid-x.cell.list-body-new > div > a')
        
         
-        for x in range(len(elementname)):
+        for x in range(len(elementname)):# ottaa tulokset
                 
                 tmp1 = f"{elementname[x].get_attribute("textContent")}\n"
                 tmp2 = f"{elementprice[x].get_attribute("innerText")}\n"
@@ -65,4 +65,3 @@ for z in range(len(webPage)):
                 f.close()
 
 driver.quit()
-#jostain syystä ei laita hintaa tai nimeä joillekin tuloksille
