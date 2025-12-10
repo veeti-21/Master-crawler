009715a75febb7ca39034fdd7aec16fa06fe9019
Joni <jonfinne@kymp.net>
2025-12-10 09:18:47 +0200

    bat tiedosto korjaus

--- scheduler.bat
+++ scheduler.bat
@@ -1,6 +1,9 @@
 @echo off
-
+python C:\crawlers\Master-crawler\oikotie_crawler.py
+python C:\crawlers\Master-crawler\crawler1.py
 python C:\crawlers\Master-crawler\crawler2.py
-
+python C:\crawlers\Master-crawler\crawler-uusiAuto.py
+python C:\crawlers\Master-crawler\main.py
+python C:\crawlers\Master-crawler\master_crawler_pelit.py
 exit
 pause
